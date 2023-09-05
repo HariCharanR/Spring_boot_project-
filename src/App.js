51bae28b887878f43c090c2bf0e2a0beefc87996
Hari Charan <hariRagu2831@gmail.com>
2023-09-05 11:38:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import Home from './Home';
+import ToyForm from './ToyForm';
 
+import OrdersForm from './OrdersForm';
 function App() {
   return (
-    <div className="border-2 border-red-500 p-5">
-      Hello there 
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/add-toy" element={<ToyForm/>}/>
+      <Route path="/:id/update-toy" element={<OrdersForm/>}/>
+
+    </Routes>
+       
+    </BrowserRouter>
   );
 }
 
